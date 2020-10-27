2395ea9573662e74670c9df486023bf235b90304
Daeng Rosanda <daeng.rosanda@yahoo.co.id>
2020-10-27 08:35:51 +0700

    FEAT: docs for controller in bahasa

--- pages/id/controller/index.vue
+++ pages/id/controller/index.vue
@@ -5,59 +5,120 @@
         <ul class="breadcrumbs">
           <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
           <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
-          <li class="unavailable">Controller Class</li>
+          <li class="unavailable">Kelas Controller</li>
         </ul>
       </nav>
       <div class="columns">
         <div class="column">
           <div class="content">
-            <h1 class="">Controller Class</h1>
-            <p>Controller is the main actor in Seme Framework life cycle.</p>
-            <p>A Controller is simply a class file that is named in a way that can be associated with an URI.</p>
-            <p>The controller class relatives to <code>app/controller</code> directory.</p>
-            <p>The controller filename and class name can only have alphanumeric with underscore.</p>
-            <h2>Example</h2>
-            <p>Here is the basic example for a controller structure:</p>
+            <h1 class="">Kelas Controller</h1>
+            <p>Kelas Controller dalam Seme Framework merupakan inti dari semua logika yang ada dalam jangkauan kelas tersebut.</p>
+            <p>Nama kelas dalam Controller juga berfungsi sebagai identitas dari sebuah alamat URL.</p>
+
+            <h2>Ketentuan Kelas Controller</h2>
+            <p>Berikut ini adalah ketentuan ketika ingin menggunakan kelas Controller dengan benar supaya dapat berjalan dengan baik:</p>
+            <ul>
+              <li>Semua kelas Controller di simpan didalam direktori <code>app/controller</code>.</li>
+              <li>
+                Kelas controller harus <code>extends</code> dari <code>SENE_Controller</code>.
+                <pre>
+app
+|-- controller
+|---- blog.php
+                </pre>
+                <pre>
+isi dari blog.php
+<code v-highlight class="php">&lt;?php class Blog extends JI_Controller { ... }</code></pre>
+
+              </li>
+              <li>
+                Dan bisa juga <code>extends</code> dari kelas <code>core</code> yang mana kelas tersebut merupakan hasil <code>extends</code> dari <code>SENE_Controller</code>.
+                <pre>
+app
+|-- core
+|---- ji_controller.php
+|-- controller
+|---- home.php
+|---- blog.php
+                </pre>
+                <pre>
+isi dari ji_controller.php
+<code v-highlight class="php">&lt;?php class JI_Controller extends SENE_Controller { ... }</code>
+isi dari home.php
+<code v-highlight class="php">&lt;?php class Home extends JI_Controller { ... }</code></pre>
+              </li>
+              <li>
+                Penamaan nama file controller dan nama kelas controller hanya boleh, diawali oleh huruf, kemudian diteruskan oleh huruf dan atau angka, dan underscore.
+                <pre>Contoh (benar):
+-nama file:
+  faktur_pajak.php
+-nama kelas:
+<code v-highlight class="php">&lt;?php class Faktur_Pajak extends SENE_Controller { .. }</code></pre>
+                <pre>Contoh (Salah):
+-nama file:
+  1faktur pajak.php
+-nama kelas:
+<code v-highlight class="php">&lt;?php class 1Faktur Pajak extends SENE_Controller { .. }</code></pre>
+              </li>
+              <li>Nama file kelas controller harus dalam huruf kecil (<i>Lower Case</i>) dan berakhiran <code>.php</code>.</li>
+              <li>
+                Nama kelas dan nama file harus identik tapi boleh tidak sama huruf besar dan atau huruf kecilnya (tidak <i>case sensitive</i>).
+                <pre>Contoh (benar):
+-nama file:
+  faktur_pajak.php
+-nama kelas:
+<code v-highlight class="php">&lt;?php class Faktur_Pajak extends SENE_Controller { .. }</code></pre>
+                <pre>Contoh (benar):
+-nama file:
+fakturpajak.php
+-nama kelas:
+<code v-highlight class="php">&lt;?php class FakturPajak extends SENE_Controller { .. }</code></pre>
+              </li>
+            </ul>
+
+
+            <h2>Contoh struktur Controller</h2>
+            <p>Berikut ini adalah contoh struktur direktori dari controller:</p>
             <pre>
 app
 |-- controller
 |---- home.php
 |---- blog.php
             </pre>
-            <p>Lets say, you have extracted Seme Framework on <code>D:\XAMPP\htdocs\seme-framework</code></p>
-            <p>So, if you accessing the <code>http://localhost/seme-framework</code> the Seme Framework will load <code>home.php</code> controller.</p>
+
+            <p>Anggap saja kita sudah mengekstrak atau clone Seme Framework ke dalam direktori <code>D:\XAMPP\htdocs\seme_framework</code>.</p>
+            <p>Jadi ketika kita membuka <code>http://localhost/seme_framework</code> maka Seme Framework akan membuka kelas controller yang ada di <code>app/controller/home.php</code>.</p>
             <b-message class="is-info">
-              <p>Class <b>home</b> or <b>home.php</b> file is the default access controller from each URI even in the sub directory.</p>
+              <p>Nama kelas <b>home</b> atau nama file <b>home.php</b> adalah kelas dasar (<i>default</i>) yang akan selalu diakses jika akar URL seperti <code>https://example.com</code>.</p>
             </b-message>
-            <p>How if, we accessing <code>http://localhost/seme-framework/blog</code>?</p>
-            <p>The answer is, Seme Framework will load controller blog.php</p>
+            <p>Bagaimana seandainya kita akan membuka alamat URL  <code>http://localhost/seme_framework/blog</code>, apakah akan muncul?</p>
+            <p>Jawabannya adalah Seme Framework akan memamnggil file controller <code>app/controller/blog.php</code> jika ada.</p>
             <p>Seme Framework will only load controller with filename and its class name are matched.</p>
 
-            <h2>SENE_Controller</h2>
-            <p>SENE_Controller is basic class for creating controller which contain properties and methods that are required for using Seme Framework properly.</p>
+            <h2>Kelas SENE_Controller</h2>
+            <p>Kelas SENE_Controller merupakan kelas dasar yang telah memiliki berbagai fungsi (method) didalamnya untuk keperluan pembangunan aplikasi berbasis web.</p>
+            <p>Ada banyak fungsi dan variabel yang telah tersedia di kelas SENE_Controller ini.</p>
 
-            <h3>Basic Usage</h3>
-            <p>Here is some basic usage for creating controller</p>
+            <h3>Penggunaan Dasar</h3>
+            <p>Berikut ini adalah cara penggunaan dasar dari Kelas Controller di Seme Framework.</p>
 
-            <h4>Default Controller</h4>
-            <p>Here is the default source code of controller.</p>
-            <pre>
-&#x3C;?php
-class Home extends Sene_Controller{
+            <h4>Controller Default</h4>
+            <p>Berikut ini adalah source code dari kelas home:</p>
+            <pre><code v-highlight class="php">&lt;?php
+class Home extends SENE_Controller{
   public function __construct(){
     parent::__construct();
   }
   public function index(){
     echo &#x27;Thankyou for using Seme Framewrok&#x27;;
   }
-}
-            </pre>
+}</code></pre>
 
-            <h4>Notfound Controller</h4>
-            <p>Here is the default source code of notfound controller.</p>
-            <pre>
-&#x3C;?php
-class Notfound extends Sene_Controller{
+            <h4>Controller untuk error 404 (not found)</h4>
+            <p>Controller not found biasanya digunakan untuk aplikasi berbasis web karena mungkin saja ada kesalah URL atau data di aplikasi sudah dihapus atau disembunyikan.</p>
+            <p>Controller not found ini sangat penting untuk menghasilkan pengalaman yang baik terhadap pengguna.</p>
+            <pre><code v-highlight class="php">&lt;?php
+class Notfound extends SENE_Controller{
   public function __construct(){
     parent::__construct();
   }
@@ -65,27 +126,24 @@ class Notfound extends Sene_Controller{
     header(&#x22;HTTP/1.0 404 Not Found&#x22;);
     echo &#x27;404 Notfound&#x27;;
   }
-}
-            </pre>
+}</code></pre>
+
             <b-message class="is-info">
-              <p>Please make sure your class name are match with filename.</p>
-            </b-message>
-            <b-message class="is-info">
-              <p>Undefined file name will be throw 404 error.</p>
+              <p><b>Perhatian</b></p>
+              <p>Pastikan bahwa nama kelas dan nama file telah identik. Apabila nama file dan nama kelas sudah identik, tapi tidak sama dengan alamat URL yang diminta maka akan menghasilkan error 404 (not found).</p>
             </b-message>
             <b-message class="is-danger">
-              <p><b>Caution</b></p>
-              <p>Mismatched classname with filename will be throw 500 error.</p>
+              <p><b>Perhatian</b></p>
+              <p>Apabila nama file cocok dengan permintaan alamat url, tapi tidak cocok dengan nama kelas. Maka akan menimbulkan error 500.</p>
             </b-message>
 
-            <h2>Access Methods</h2>
-            <p>Seme Framework supported access method for public and private declaration.</p>
-            <h3>Private Properties and Methods</h3>
-            <p>Seme Framework allow your method in Private or in Protected mode. The Private or Protected mode cant be executed through URI request. Or you can use double underscore (__) means as Private properties or methods.</p>
-            <pre>
-class Home extends Sene_Controller{
-  var $public_var = &#x27;Yi-Ha!&#x27;;
-  var $__underWear = &#x27;My Private Things&#x27;;
+            <h2>Pembatasan akses pada method dan properti</h2>
+            <p>Seme Framework telah mendukung deklrasi untuk private method, private properti, protected method dan protected properti.</p>
+
+            <pre><code v-highlight class="php">&lt;?php
+class Home extends SENE_Controller{
+  var $public_var = &#039;Yi-Ha!&#039;
+  var $__underWear = &#039;My Private Things&#039;
   public function __construct(){
     parent::__construct();
   }
@@ -93,15 +151,14 @@ class Home extends Sene_Controller{
     return true;
   }
   public function index(){
-    echo &#x27;404 Notfound&#x27;;
+    echo &#039;not found&#039;
   }
-}
-            </pre>
+}</code></pre>
             <b-message class="is-info">
-              <p>The private method or private properties on Seme Framework are always prefixed with double underscore.</p>
+              <p>Private method dalam Seme Framework diawali dengan underscore 2 kali (__).</p>
             </b-message>
             <b-message class="is-success">
-              <p>The private method is inaccessible from URI request.</p>
+              <p>Private method tidak dapat diakses oleh alamat URL langsung.</p>
             </b-message>
 
           </div>
