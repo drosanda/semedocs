63344a67a146f5e1caef5f980cc6aa02f06576d5
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-24 14:25:55 +0700

    FEAT: add new example codes

--- pages/3.1.0/model/query.vue
+++ pages/3.1.0/model/query.vue
@@ -22,18 +22,45 @@
 					<p>Here is the examples using SQL method. See the first of this page for full example.</p>
           <h3>Basic Usage</h3>
           <p>For example we assumed want to add new data in blog table. First, in the model:</p>
-          <pre>class Blog_Model extends SENE_Model{
-	var $tbl = 'd_order';
-	var $tbl_as = 'dor';
-	public function __construct(){
-		parent::__construct();
-		$this->db->from($this->tbl,$this->tbl_as);
-	}
-	public function getReported($id){
-    $sql = 'SELECT bk.name AS category FROM blog WHERE is_published = 1 ORDER BY created_date DESC LIMIT 0,1;';
-		return $this->db->query($sql);
-	}
-}</pre>
+          <div class="macwindow">
+            <div class="titlebar">
+              <div class="buttons">
+                <div class="close">
+                  <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                  <!-- close button link -->
+                </div>
+                <div class="minimize">
+                  <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                  <!-- minimize button link -->
+                </div>
+                <div class="zoom">
+                  <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                  <!-- zoom button link -->
+                </div>
+              </div>
+            </div>
+            <div class="maccontent">
+              <highlight-code lang="php">
+                &#x3C;?php
+                class Blog_Model extends SENE_Model{
+                  var $tbl = &#x27;d_blog&#x27;;
+                  var $tbl_as = &#x27;dbl&#x27;;
+                  public function __construct(){
+                    parent::__construct();
+                    $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                  }
+                  public function getLatePublish($id){
+                    $sql = &#x27;SELECT `title`, `pubdt` AS 'datePublished'
+                    FROM &#x27;.$this-&#x3E;tbl.&#x27; &#x27;.$this-&#x3E;tbl_as.&#x27;
+                    WHERE is_published = 1
+                    ORDER BY cdate DESC
+                    LIMIT 0,1;&#x27;;
+                    return $this-&#x3E;db-&#x3E;query($sql);
+                  }
+                }
+              </highlight-code>
+            </div>
+          </div>
 
         </div>
       </div>
