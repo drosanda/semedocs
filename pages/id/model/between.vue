c0140fc89159960a3f02349b3ffa8d5061bf3c31
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-24 14:33:05 +0700

    FEAT: last_id

--- pages/id/model/between.vue
+++ pages/id/model/between.vue
@@ -203,61 +203,62 @@ export default {
       ]
     }
   },
-  jsonld() { this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
-  const items = this.breadcrumbs.map((item, index) => ({
-    '@type': 'ListItem',
-    position: index + 1,
-    item: {
-      '@id': item.url,
-      name: item.text,
-    },
-  }));
-  return [
-    {
-      '@context': 'https://schema.org',
-      '@type': 'BreadcrumbList',
-      itemListElement: items,
-    },
-    {
-      "@type": "NewsArticle",
-      "mainEntityOfPage": {
-        "@type": "WebPage",
-        "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
       },
-      "headline": (this.headline || this.title),
-      "image": [
-        (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
-      ],
-      "dateCreated": "2020-06-11T10:12:00+07:00",
-      "datePublished": "2020-06-11T10:12:00+07:00",
-      "dateModified": "2021-06-24T12:54:00+07:00",
-      "author": {
-        "@type": "Person",
-        "gender": "Male",
-        "name": "Daeng Rosanda, S.Kom",
-        "alternateName": "Daeng Rosanda",
-        "jobTitle": "Founder",
-        "worksFor": {
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2020-06-11T10:12:00+07:00",
+        "datePublished": "2020-06-11T10:12:00+07:00",
+        "dateModified": "2021-06-24T12:54:00+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
           "@type": "Organization",
           "name": "Cipta Esensi Merenah",
-          "email": "hi@cenah.co.id"
-        }
-      },
-      "publisher": {
-        "@type": "Organization",
-        "name": "Cipta Esensi Merenah",
-        "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
-        "logo": {
-          "@type": "ImageObject",
-          "name": "logo Cipta Esensi Merenah",
-          "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
-          "width": "256px",
-          "height": "62px"
-        }
-      },
-      "description": this.description
-    }
-  ];
-}
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
+  }
 }
 </script>
--- pages/id/model/last_id.vue
+++ pages/id/model/last_id.vue
@@ -1,63 +1,215 @@
 <template>
-    <div class="section">
-        <div class="container">
-          <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul class="breadcrumbs">
-              <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
-<li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
-              <li class=""><NuxtLink to="/id/model">Model</NuxtLink></li>
-            <li class="unavailable">Last ID Method</li>
-          </ul>
-        </nav>
-        <div class="columns">
-          <div class="column">
-        <div class="content">
-          <h1 class="">Last ID Method</h1>
-					<p>Last ID method is part of database class builder that returned postive integer value if an ID has generated automatically from auto increment feature.</p>
-					<h2>Parameters</h2>
-					<p>Last ID method has no parameter.</p>
-					<pre>$this->db->lastId(): integer</pre>
-          <h2>Example usage</h2>
-					<p>Here is the examples using Last ID method. See the first of this page for full example.</p>
-          <h3>Basic Usage</h3>
-          <p>For example we assumed want to add new data in blog table. First, in the model:</p>
-          <pre>class Blog_Model extends SENE_Model{
-  var $tbl = 'blog';
-  var $tbl_as = 'b';
-  public function __construct(){
-	 parent::__construct();
-  }
-  public function insert($di){
-    $this->db->insert($ths->tbl,$di);
-    return $this->db->lastId();
-  }
-}</pre>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/model">Model</NuxtLink></li>
+          <li class="unavailable">Last ID Method</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">Metode Last ID</h1>
+            <p>
+              Metode last_id digunakan untuk mengambil ID terakhir data yang ditambahkan tabel dalam database.
+              Metode ini mengembalikan nilai berupa angka (integer) positif.
+              Metode ini hanya akan berjalan dengan baik apabila kolom pada table sudah disetel sebagi AUTO_INCREMENT.
+            </p>
+
+            <h2>Parameters</h2>
+            <p>Metode ini tidak membutuhkan parameter apapun</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this-&#x3E;db-&#x3E;lastId(): integer
+                </highlight-code>
+              </div>
+            </div>
+
+            <h2>Contoh Penggnaan</h2>
+            <p>Berikut ini adalah contoh penggunaan dalam bentuk kode untuk metode last_id.</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  &#x3C;?php
+                  class D_Blog_Model extends SENE_Model{
+                    var $tbl = &#x27;d_blog&#x27;;
+                    var $tbl_as = &#x27;dbl&#x27;;
+                    public function __construct(){
+                      &#x9; parent::__construct();
+                    }
+                    public function insert($di){
+                      $this-&#x3E;db-&#x3E;insert($ths-&#x3E;tbl,$di);
+                      return $this-&#x3E;db-&#x3E;lastId();
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="nav-bottom">
+        <div class="nav-bottom-left">
+          <nuxt-link to="/id/model/query/" class="btn">
+            <i class="fa fa-chevron-left"></i>
+            Model::query
+          </nuxt-link>
+        </div>
+        <div class="nav-bottom-right">
+          <nuxt-link to="/id/view/" class="btn">
+            View
+            <i class="fa fa-chevron-right"></i>
+          </nuxt-link>
         </div>
       </div>
 
-        </div>
-
-
-        <div class="columns">
-          <div class="column">
-            <b-button tag="router-link" to="/id/model/query" icon-pack="fa" icon-left="chevron-left" class="is-pulled-left">
-              Model::query
-            </b-button>
-          </div>
-          <div class="column is-2">&nbsp;</div>
-          <div class="column">
-            <b-button tag="router-link" to="/id/view" icon-pack="fa" icon-right="chevron-right" class="is-pulled-right">
-              View
-            </b-button>
-          </div>
-        </div>
-
-      </div>
     </div>
-  </template>
+  </div>
+</template>
 <script>
-  export default {
-    layout: 'v4.0.0'
-    // page component definitions
+export default {
+  layout: 'id',
+  data (){
+    return {
+      name: 'Seme Framework 4',
+      suffix: ' - Seme Framework 4 Bahasa Indonesia',
+      title: 'Metode Last ID',
+      description: 'Pelajari selengkapnya tentang Metode Last ID di Seme Framework versi 4.0.0',
+      breadcrumbs: [
+        {
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework',
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/',
+          text: 'ID',
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/model/',
+          text: 'Model',
+        }
+      ],
+    }
+  },
+  head() {
+    return {
+      htmlAttrs: {
+        lang: 'id'
+      },
+      title: this.title+this.suffix,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.name+': '+this.title
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.description
+        }
+      ]
+    }
+  },
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
+      },
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2020-06-11T10:12:00+07:00",
+        "datePublished": "2020-06-11T10:12:00+07:00",
+        "dateModified": "2021-06-24T12:54:00+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
   }
+}
 </script>
