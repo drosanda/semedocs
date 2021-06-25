c1f6a73a76f02805cf35f6f4b510e0d0a3ea26d8
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-25 13:42:12 +0700

    FIX: breadcrumb

--- pages/id/index.vue
+++ pages/id/index.vue
@@ -122,7 +122,7 @@
             text: 'Seme Framework',
           },
           {
-            url: process.env.BASE_URL || 'http://localhost:3001/id/',
+            url: (process.env.BASE_URL || 'http://localhost:3001') + '/id/',
             text: 'ID',
           }
         ]
