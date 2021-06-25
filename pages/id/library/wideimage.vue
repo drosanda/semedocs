407400c0f857d583e86cae93c7603bd95a8de6c6
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-25 11:45:30 +0700

    FEAT: library wideimage ID

---
+++ pages/id/library/wideimage.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/library">Library</NuxtLink></li>
+          <li class="unavailable">WideImage</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">WideImage</h1>
+            <p>
+              Untuk penggunaan library WideImage secara lengkap bisa membacanya melalui <a href="http://wideimage.sourceforge.net/" target="_blank">website resminya (dalam bahasa inggris) <i class="fa fa-external-link"></i></a>.
+            </p>
+
+            <div class="nav-bottom">
+              <div class="nav-bottom-left">
+                <nuxt-link to="/id/library/seme_log/" class="btn">
+                <i class="fa fa-chevron-left"></i>
+                  Seme_Log
+                </nuxt-link>
+              </div>
+              <div class="nav-bottom-right">
+                <nuxt-link to="/id/library/" class="btn">
+                  Library
+                  <i class="fa fa-chevron-right"></i>
+                </nuxt-link>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  </template>
+  <script>
+  export default {
+    layout: 'id',
+    data() {
+      return {
+        name: 'Seme Framework 4',
+        suffix: ' - Seme Framework 4 Bahasa Indonesia',
+        title: 'Penggunaan WideImage',
+        description: 'Pelajari selengkapnya tentang Penggunaan WideImage di Seme Framework versi 4.0.0',
+        breadcrumbs: [
+          {
+            url: process.env.BASE_URL || 'http://localhost:3001',
+            text: 'Seme Framework',
+          },
+          {
+            url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/',
+            text: 'ID',
+          },
+          {
+            url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/library/',
+            text: 'Library',
+          }
+        ]
+      }
+    },
+    head() {
+      return {
+        title: this.title+this.suffix,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: this.description
+          },
+          {
+            hid: 'og:title',
+            name: 'og:title',
+            content: this.name+': '+this.title
+          },
+          {
+            hid: 'og:description',
+            name: 'og:description',
+            content: this.description
+          }
+        ]
+      }
+    },
+    jsonld() {
+      this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+      const items = this.breadcrumbs.map((item, index) => ({
+        '@type': 'ListItem',
+        position: index + 1,
+        item: {
+          "@type": "WebPage",
+          '@id': item.url,
+          name: item.text,
+        },
+      }));
+      return [
+        {
+          '@context': 'https://schema.org',
+          '@type': 'BreadcrumbList',
+          itemListElement: items,
+        },
+        {
+          "@type": "NewsArticle",
+          "mainEntityOfPage": {
+            "@type": "WebPage",
+            "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+          },
+          "headline": (this.headline || this.title),
+          "image": [
+            (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+          ],
+          "dateCreated": "2020-06-25T11:37:00+07:00",
+          "datePublished": "2020-06-25T11:37:20+07:00",
+          "dateModified": "2021-06-25T11:39:00+07:00",
+          "author": {
+            "@type": "Person",
+            "gender": "Male",
+            "name": "Daeng Rosanda, S.Kom",
+            "alternateName": "Daeng Rosanda",
+            "jobTitle": "Founder",
+            "worksFor": {
+              "@type": "Organization",
+              "name": "Cipta Esensi Merenah",
+              "email": "hi@cenah.co.id"
+            }
+          },
+          "publisher": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+            "logo": {
+              "@type": "ImageObject",
+              "name": "logo Cipta Esensi Merenah",
+              "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+              "width": "256px",
+              "height": "62px"
+            }
+          },
+          "description": this.description
+        }
+      ];
+    }
+  }
+  </script>
