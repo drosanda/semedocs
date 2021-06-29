6a4be5c92a845e7df64c65738a9ca9fbb74cd060
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-06-29 23:50:13 +0700

    FEAT: add button nav

--- pages/id/model/from.vue
+++ pages/id/model/from.vue
@@ -6,17 +6,39 @@
           <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
           <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
           <li class=""><NuxtLink to="/id/model">Model</NuxtLink></li>
-          <li class="unavailable">From Method</li>
+          <li class="unavailable">Metode from</li>
         </ul>
       </nav>
       <div class="columns">
         <div class="column">
           <div class="content">
-            <h1 class="">From Method</h1>
+            <h1 class="">Metode from</h1>
             <p><b>From</b> method is part of database class builder for selecting table with its alias.</p>
             <h2>Parameters</h2>
             <p>Update method has 2 required parameters that is <b>table name</b> and <b>table alias</b>. Table alias are required for using <b>join</b> and <b>join_method</b></p>
-            <pre>$this->db->from(string $table_name, string $table_alias): object</pre>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this->db->from(string $table_name, string $table_alias): object
+                </highlight-code>
+              </div>
+            </div>
             <h3>$table_name</h3>
             <p><b>Table Name</b> refer to table name.</p>
             <h3>$alias</h3>
@@ -25,150 +47,172 @@
             <p>Here is the examples using select method. See the first of this page for full example.</p>
             <h3>Basic Usage</h3>
             <p>For example we assumed want to add new data in blog table. First, in the model:</p>
-            <pre>
-class Blog_Model extends SENE_Model{
-  var $tbl = &#x27;blog&#x27;;
-  var $tbl_as = &#x27;b&#x27;;
-  public function __construct(){
-    parent::__construct();
-  }
-  public function countList(){
-    $this-&#x3E;db-&#x3E;select_as(&#x22;COUNT(*)&#x22;,&#x22;total&#x22;,0);
-    $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
-    return $this-&#x3E;db-&#x3E;get_first();
-  }
-}
-            </pre>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  class Blog_Model extends SENE_Model{
+                    var $tbl = &#x27;blog&#x27;;
+                    var $tbl_as = &#x27;b&#x27;;
+                    public function __construct(){
+                      parent::__construct();
+                    }
+                    public function countList(){
+                      $this-&#x3E;db-&#x3E;select_as(&#x22;COUNT(*)&#x22;,&#x22;total&#x22;,0);
+                      $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                      return $this-&#x3E;db-&#x3E;get_first();
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
 
           </div>
         </div>
       </div>
 
-      <div class="columns">
-        <div class="column">
-          <b-button tag="router-link" to="/id/model/select_as/" icon-pack="fa" icon-left="chevron-left" class="is-pulled-left">
-            Model::select_as
-          </b-button>
+      <div class="nav-bottom">
+        <div class="nav-bottom-left">
+          <nuxt-link to="/id/model/exec/" class="btn">
+            <i class="fa fa-chevron-left"></i>
+            Metode exec
+          </nuxt-link>
         </div>
-        <div class="column is-2">&nbsp;</div>
-        <div class="column">
-          <b-button tag="router-link" to="/id/model/join" icon-pack="fa" icon-right="chevron-right" class="is-pulled-right">
-            Model::join
-          </b-button>
+        <div class="nav-bottom-right">
+          <nuxt-link to="/id/model/get_first/" class="btn">
+            Metode get_first
+            <i class="fa fa-chevron-right"></i>
+          </nuxt-link>
         </div>
       </div>
+
     </div>
   </div>
-  </template>
-  <script>
-  export default {
-    layout: 'id',
-    data (){
-      return {
-        name: 'Seme Framework 4',
-        suffix: ' - Seme Framework 4',
-        title: 'Model::from method',
-        description: 'Pelajari selengkapnya tentang from method on SENE_Model class from Seme Framework.',
-        breadcrumbs: [
-          {
-            url: process.env.BASE_URL || 'http://localhost:3001',
-            text: 'Seme Framework',
-          },
-          {
-            url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/',
-            text: 'ID',
-          },
-          {
-            url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/tutorial/',
-            text: 'Model',
-          },
-          {
-            url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/tutorial/introduction/',
-            text: 'From',
-          },
-        ],
-      }
-    },
-    head() {
-      return {
-        htmlAttrs: {
-          lang: 'id'
-        },
-        title: this.title+this.suffix,
-        meta: [
-          {
-            hid: 'description',
-            name: 'description',
-            content: this.description
-          },
-          {
-            hid: 'og:title',
-            name: 'og:title',
-            content: this.name+': '+this.title
-          },
-          {
-            hid: 'og:description',
-            name: 'og:description',
-            content: this.description
-          }
-        ]
-      }
-    },
-    jsonld() { this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
-      const items = this.breadcrumbs.map((item, index) => ({
-        '@type': 'ListItem',
-        position: index + 1,
-        item: {
-          '@id': item.url,
-          name: item.text,
-        },
-      }));
-      return [
+</template>
+<script>
+export default {
+  layout: 'id',
+  data (){
+    return {
+      name: 'Seme Framework 4',
+      suffix: ' - Seme Framework 4',
+      title: 'Metode from',
+      description: 'Pelajari selengkapnya tentang Metode from dari SENE_Model di Seme Framework 4.0.0',
+      breadcrumbs: [
         {
-          '@context': 'https://schema.org',
-          '@type': 'BreadcrumbList',
-          itemListElement: items,
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework',
         },
         {
-          "@type": "NewsArticle",
-          "mainEntityOfPage": {
-            "@type": "WebPage",
-            "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
-          },
-          "headline": (this.headline || this.title),
-          "image": [
-            (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
-          ],
-          "dateCreated": "2020-06-11T10:12:00+07:00",
-          "datePublished": "2020-06-11T10:12:00+07:00",
-          "dateModified": "2021-06-11T01:04:00+07:00",
-          "author": {
-            "@type": "Person",
-            "gender": "Male",
-            "name": "Daeng Rosanda, S.Kom",
-            "alternateName": "Daeng Rosanda",
-            "jobTitle": "Founder",
-            "worksFor": {
-              "@type": "Organization",
-              "name": "Cipta Esensi Merenah",
-              "email": "hi@cenah.co.id"
-            }
-          },
-          "publisher": {
-            "@type": "Organization",
-            "name": "Cipta Esensi Merenah",
-            "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
-            "logo": {
-              "@type": "ImageObject",
-              "name": "logo Cipta Esensi Merenah",
-              "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
-              "width": "256px",
-              "height": "62px"
-            }
-          },
-          "description": this.description
-        }
-      ];
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/',
+          text: 'ID',
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/tutorial/',
+          text: 'Model',
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/tutorial/introduction/',
+          text: 'From',
+        },
+      ],
     }
-  }
-  </script>
+  },
+  head() {
+    return {
+      htmlAttrs: {
+        lang: 'id'
+      },
+      title: this.title+this.suffix,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.name+': '+this.title
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.description
+        }
+      ]
+    }
+  },
+  jsonld() { this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+  const items = this.breadcrumbs.map((item, index) => ({
+    '@type': 'ListItem',
+    position: index + 1,
+    item: {
+      '@id': item.url,
+      name: item.text,
+    },
+  }));
+  return [
+    {
+      '@context': 'https://schema.org',
+      '@type': 'BreadcrumbList',
+      itemListElement: items,
+    },
+    {
+      "@type": "NewsArticle",
+      "mainEntityOfPage": {
+        "@type": "WebPage",
+        "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+      },
+      "headline": (this.headline || this.title),
+      "image": [
+        (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+      ],
+      "dateCreated": "2020-06-11T10:12:00+07:00",
+      "datePublished": "2020-06-11T10:12:00+07:00",
+      "dateModified": "2021-06-11T01:04:00+07:00",
+      "author": {
+        "@type": "Person",
+        "gender": "Male",
+        "name": "Daeng Rosanda, S.Kom",
+        "alternateName": "Daeng Rosanda",
+        "jobTitle": "Founder",
+        "worksFor": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "email": "hi@cenah.co.id"
+        }
+      },
+      "publisher": {
+        "@type": "Organization",
+        "name": "Cipta Esensi Merenah",
+        "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+        "logo": {
+          "@type": "ImageObject",
+          "name": "logo Cipta Esensi Merenah",
+          "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+          "width": "256px",
+          "height": "62px"
+        }
+      },
+      "description": this.description
+    }
+  ];
+}
+}
+</script>
--- pages/id/model/get_first.vue
+++ pages/id/model/get_first.vue
@@ -1,83 +1,127 @@
 <template>
-    <div class="section">
-        <div class="container">
-          <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul class="breadcrumbs">
-              <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
-              <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
-              <li class=""><NuxtLink to="/id/model">Model</NuxtLink></li>
-            <li class="unavailable">Get First Method</li>
-          </ul>
-        </nav>
-        <div class="columns">
-          <div class="column">
-        <div class="content">
-          <h1 class="">Get First Method</h1>
-					<p>Get First method is part of database class builder for filtering just only one result.</p>
-					<h2>Parameters</h2>
-					<p>Group By method has 2 parameter</p>
-					<pre>$this->db->get_first([string $result_type, [bool $is_debug]]): mixed</pre>
-          <h3>$result_type</h3>
-          <p><b>Result type</b> consist of array and object, if value has "array" the result will be array otherwise will returned object.</p>
-					<h2>Example usage</h2>
-					<p>Here is the examples using select method. See the first of this page for full example.</p>
-          <h3>Basic Usage</h3>
-          <p>For example we assumed want to add new data in blog table. First, in the model:</p>
-          <pre>
-class Blog_Model extends SENE_Model{
-  var $tbl = &#x27;blog&#x27;;
-  var $tbl_as = &#x27;b&#x27;;
-  public function __construct(){
-&#x9; parent::__construct();
-  }
-  public function getById($id){
-    $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
-    $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
-    $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
-    return $this-&#x3E;db-&#x3E;get_first();
-  }
-  public function getByIdArray($id){
-    $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
-    $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
-    $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
-    return $this-&#x3E;db-&#x3E;get_first(&#x27;array&#x27;);
-  }
-  public function getByIdArrayDebugQuery($id){
-    $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
-    $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
-    $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
-    $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
-    return $this-&#x3E;db-&#x3E;get_first(&#x27;array&#x27;,1);
-  }
-}
-          </pre>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/">4.0.0 (Bahasa)</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/model">Model</NuxtLink></li>
+          <li class="unavailable">Get First Method</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">Get First Method</h1>
+            <p>Get First method is part of database class builder for filtering just only one result.</p>
+            <h2>Parameters</h2>
+            <p>Group By method has 2 parameter</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this->db->get_first([string $result_type, [bool $is_debug]]): mixed
+                </highlight-code>
+              </div>
+            </div>
+
+            <h3>$result_type</h3>
+            <p><b>Result type</b> consist of array and object, if value has "array" the result will be array otherwise will returned object.</p>
+            <h2>Example usage</h2>
+            <p>Here is the examples using select method. See the first of this page for full example.</p>
+            <h3>Basic Usage</h3>
+            <p>For example we assumed want to add new data in blog table. First, in the model:</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  class Blog_Model extends SENE_Model{
+                    var $tbl = &#x27;blog&#x27;;
+                    var $tbl_as = &#x27;b&#x27;;
+                    public function __construct(){
+                      &#x9; parent::__construct();
+                    }
+                    public function getById($id){
+                      $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
+                      $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                      $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
+                      return $this-&#x3E;db-&#x3E;get_first();
+                    }
+                    public function getByIdArray($id){
+                      $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
+                      $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                      $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
+                      return $this-&#x3E;db-&#x3E;get_first(&#x27;array&#x27;);
+                    }
+                    public function getByIdArrayDebugQuery($id){
+                      $this-&#x3E;db-&#x3E;select(&#x22;id&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;title&#x22;);
+                      $this-&#x3E;db-&#x3E;select(&#x22;content&#x22;);
+                      $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                      $this-&#x3E;db-&#x3E;where_as(&#x22;id&#x22;,$id);
+                      return $this-&#x3E;db-&#x3E;get_first(&#x27;array&#x27;,1);
+                    }
+                  }
+
+                </highlight-code>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div class="nav-bottom">
+        <div class="nav-bottom-left">
+          <nuxt-link to="/id/model/fromt/" class="btn">
+            <i class="fa fa-chevron-left"></i>
+            Metode from
+          </nuxt-link>
+        </div>
+        <div class="nav-bottom-right">
+          <nuxt-link to="/id/model/get/" class="btn">
+            Metode get
+            <i class="fa fa-chevron-right"></i>
+          </nuxt-link>
         </div>
       </div>
 
-        </div>
-
-        <div class="columns">
-          <div class="column">
-            <b-button tag="router-link" to="/id/model/get" icon-pack="fa" icon-left="chevron-left" class="is-pulled-left">
-              Model::get
-            </b-button>
-          </div>
-          <div class="column is-2">&nbsp;</div>
-          <div class="column">
-            <b-button tag="router-link" to="/id/model/insert" icon-pack="fa" icon-right="chevron-right" class="is-pulled-right">
-              Model::insert
-            </b-button>
-          </div>
-        </div>
-
-      </div>
     </div>
-  </template>
+  </div>
+</template>
 <script>
 export default {
   layout: 'id',
@@ -85,8 +129,8 @@ export default {
     return {
       name: 'Seme Framework 4',
       suffix: ' - Seme Framework 4',
-      title: 'Model::get_first method',
-      description: 'Pelajari selengkapnya tentang get_first method on SENE_Model class from Seme Framework.',
+      title: 'Metode get_first',
+      description: 'Pelajari selengkapnya tentang Metode get_first dari SENE_Model di Seme Framework 4.0.0',
       breadcrumbs: [
         {
           url: process.env.BASE_URL || 'http://localhost:3001',
@@ -132,7 +176,8 @@ export default {
       ]
     }
   },
-  jsonld() { this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
     const items = this.breadcrumbs.map((item, index) => ({
       '@type': 'ListItem',
       position: index + 1,
