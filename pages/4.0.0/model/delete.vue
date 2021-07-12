848ecba65ec9264136d5fbfee93a84b627aefd97
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-07-12 20:33:15 +0700

    FEAT: delete method

--- pages/4.0.0/model/delete.vue
+++ pages/4.0.0/model/delete.vue
@@ -1,63 +1,116 @@
 <template>
-    <div class="section">
-        <div class="container">
-          <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul class="breadcrumbs">
-              <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
-<li class=""><NuxtLink to="/4.0.0/">4.0.0</NuxtLink></li>
-              <li class=""><NuxtLink to="/4.0.0/model">Model</NuxtLink></li>
-            <li class="unavailable">Delete Method</li>
-          </ul>
-        </nav>
-        <div class="columns">
-          <div class="column">
-        <div class="content">
-          <h1 class="">Delete Method</h1>
-					<p>Delete method for deleting data in the table.</p>
-					<pre>$this->db->delete(string $table_name [, int $is_debug=0]): boolean</pre>
-          <h2>Parameters</h2>
-          <p>Where method has 2 parameters that is <b>table name</b> and <b>debug flag</b>.</p>
-          <h3>$table_name</h3>
-          <p><b>Table Name</b> can be string contain table name.</p>
-					<h2>Example usage</h2>
-					<p>Here is the examples using <b>delete method</b>. See the first of this page for full example.</p>
-          <h3>Basic Usage</h3>
-          <p>For example we assumed want to delete data in blog table. First, in the model:</p>
-<pre>class Blog_Model extends SENE_Model{
-	var $tbl = 'd_order';
-	var $tbl_as = 'dor';
-	public function __construct(){
-		parent::__construct();
-		$this->db->from($this->tbl,$this->tbl_as);
-	}
-	public function delete($id){
-    $this->db->where("id",$id);
-		return $this->db->delete($this->tbl);
-	}
-}
-					</pre>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/4.0.0/">4.0.0</NuxtLink></li>
+          <li class=""><NuxtLink to="/4.0.0/model">Model</NuxtLink></li>
+          <li class="unavailable">Delete Method</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">Delete Method</h1>
+            <p>Delete method for deleting data in the table.</p>
 
+            <h2>Basic Usage</h2>
+            <p>
+              Here is basic usage for delete method.
+            </p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this->db->delete(string $table_name [, int $is_debug=0]): boolean
+                </highlight-code>
+              </div>
+            </div>
+
+            <h3>Parameters</h3>
+            <p>Where method has 2 parameters that is <b>$table_name</b> and <b>$is_debug</b>.</p>
+
+            <h4>$table_name</h4>
+            <p>The name of table that will be deleted.</p>
+
+            <h4>$is_debug</h4>
+            <p>The debug flag. If value is 1, the process will be halted and then the query will be showed (printed).</p>
+
+            <h2>Example Usage</h2>
+            <p>Here is the examples using <b>delete method</b> on blog_model.php file</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  class Blog_Model extends SENE_Model{
+                    var $tbl = 'd_order';
+                    var $tbl_as = 'dor';
+                    public function __construct(){
+                      parent::__construct();
+                      $this->db->from($this->tbl,$this->tbl_as);
+                    }
+                    public function delete($id){
+                      $this->db->where("id",$id);
+                      return $this->db->delete($this->tbl);
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
-        </div>
 
-        <div class="columns">
-          <div class="column">
-            <b-button tag="router-link" to="/4.0.0/model/update" icon-pack="fa" icon-left="chevron-left" class="is-pulled-left">
-              Model::update
-            </b-button>
-          </div>
-          <div class="column is-2">&nbsp;</div>
-          <div class="column">
-            <b-button tag="router-link" to="/4.0.0/model/exec" icon-pack="fa" icon-right="chevron-right" class="is-pulled-right">
-              Model::exec
-            </b-button>
-          </div>
+      <div class="nav-bottom">
+        <div class="nav-bottom-left">
+          <nuxt-link to="/4.0.0/model/between/" class="btn">
+            <i class="fa fa-chevron-left"></i>
+            Metode composite_create
+          </nuxt-link>
         </div>
-
+        <div class="nav-bottom-right">
+          <nuxt-link to="/4.0.0/model/exec/" class="btn">
+            Metode exec
+            <i class="fa fa-chevron-right"></i>
+          </nuxt-link>
+        </div>
       </div>
+
     </div>
-  </template>
+  </div>
+</template>
 <script>
 export default {
   layout: 'v4.0.0',
@@ -65,8 +118,22 @@ export default {
     return {
       name: 'Seme Framework v4.0.0',
       suffix: ' - Seme Framework 4',
-      title: 'Model::delete method',
-      description: 'Learn more about delete method on SENE_Model class from Seme Framework.'
+      title: 'Delete method',
+      description: 'Learn more about Delete method on SENE_Model class Seme Framework.',
+      breadcrumbs: [
+        {
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/4.0.0/',
+          text: '4.0.0'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/tutorial/',
+          text: 'Model'
+        }
+      ],
     }
   },
   head() {
@@ -90,6 +157,63 @@ export default {
         }
       ]
     }
+  },
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
+      },
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2021-07-12T20:31:00+07:00",
+        "datePublished": "2021-07-12T20:31:07+07:00",
+        "dateModified": "2021-07-12T20:32:00+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
   }
 }
 </script>
