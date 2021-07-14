79909efee87710e61402f0c538b8e1598aa4bbd4
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-07-14 19:14:29 +0700

    FEAT: enhance setTheme

--- pages/4.0.0/controller/setTheme.vue
+++ pages/4.0.0/controller/setTheme.vue
@@ -14,61 +14,159 @@
           <div class="content">
             <h1 class="">setTheme Method</h1>
             <p>This method will be loaded theme and overrides the current theme value using the new one.</p>
-            <p>The <code>$theme_name</code> value will be reffered to a directory name under <code>app/view/</code>.</p>
 
-            <h2>Theme Requirements</h2>
+            <h2>Basic Usage</h2>
+            <p>Here is the basic usage of <code>setTheme</code> method.</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  setTheme(string $theme_name): controllerObject
+                </highlight-code>
+              </div>
+            </div>
+
+            <h3>Parameters</h3>
+            <p>
+              setTheme requires 1 parameter it is $theme_name.
+            </p>
+
+            <h4>$theme_name</h4>
+            <p>
+              Theme name value can be a directory name under <code>app/view</code>.
+            </p>
+            <h5>Theme Name Requirements</h5>
             <p>The valid themes should contain these files with this directory structure.</p>
-            <pre>
-|--- theme.json
-|--- script.json
-|--- page
-|----- col-1.php
-            </pre>
-            <h2>Basic Usage:</h2>
-            <p>Here is the basic usage of <code>Controller::setTheme</code>.</p>
-            <pre>
-Controller::setTheme(string $theme_name): ControllerObject
-            </pre>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  | app/
+                  |- view/
+                  |-- THEME_NAME/
+                  |--- theme.json
+                  |--- script.json
+                  |--- page/
+                  |----- col-1.php
+                </highlight-code>
+              </div>
+            </div>
 
             <h2>Example</h2>
-            <p>Here is the example for <code>setTheme</code> method:</p>
-            <pre>
-class Home extends SENE_Controller
-{
-  public function __construct()
-  {
-    parent::__construct();
-    $this-&#x3E;setTheme(&#x27;homepage&#x27;);
-  }
-  public function index()
-  {
-    ...
-  }
-}
-            </pre>
+            <p>Here is the example for <code>setTheme</code> method.</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  class Home extends SENE_Controller
+                  {
+                    public function __construct()
+                    {
+                      parent::__construct();
+                      $this-&#x3E;setTheme(&#x27;homepage&#x27;);
+                    }
+                    public function index()
+                    {
+                      ...
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
             <p>So, the directory <code>homepage</code> should be existed on the directory structure.</p>
-            <pre>
-- app
-|-- view
-|--- front
-|--- homepage
-            </pre>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  | app/
+                  |- view/
+                  |-- homepage/
+                  |--- theme.json
+                  |--- script.json
+                  |--- page/
+                  |----- col-1.php
+                </highlight-code>
+              </div>
+            </div>
+
+            <div class="nav-bottom">
+              <div class="nav-bottom-left">
+                <nuxt-link to="/4.0.0/controller/setshortcuticon/" class="btn">
+                  <i class="fa fa-chevron-left"></i>
+                  setShortcutIcon
+                </nuxt-link>
+              </div>
+              <div class="nav-bottom-right">
+                <nuxt-link to="/4.0.0/controller/settitle/" class="btn">
+                  setTitle
+                  <i class="fa fa-chevron-right"></i>
+                </nuxt-link>
+              </div>
+            </div>
+
           </div>
         </div>
 
-      </div>
-
-      <div class="columns">
-        <div class="column">
-          <div class="buttons">
-            <b-button tag="router-link" to="/4.0.0/controller/input" icon-pack="fa" icon-left="chevron-left">
-              Input Method
-            </b-button>
-            <b-button tag="router-link" to="/4.0.0/controller/loadLayout" icon-pack="fa" icon-right="chevron-right">
-              loadLayout Method
-            </b-button>
-          </div>
-        </div>
       </div>
 
     </div>
@@ -82,7 +180,21 @@ export default {
       name: 'Seme Framework v4.0.0',
       suffix: ' - Seme Framework 4',
       title: 'setTheme Method from SENE_Controller',
-      description: 'Learn more about setTheme Method from SENE_Controller on Seme Framework version 4.0.0'
+      description: 'Learn more about setTheme Method from SENE_Controller on Seme Framework version 4.0.0',
+      breadcrumbs: [
+        {
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/4.0.0/',
+          text: '4.0.0'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/4.0.0/controller/',
+          text: 'Controller'
+        }
+      ],
     }
   },
   head() {
@@ -106,6 +218,63 @@ export default {
         }
       ]
     }
+  },
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
+      },
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2021-07-14T19:01:00+07:00",
+        "datePublished": "2021-07-14T19:01:00+07:00",
+        "dateModified": "2021-07-14T19:09:00+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
   }
 }
 </script>
