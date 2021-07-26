305a73c27b0df852483babb99829cb92562a643e
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-07-26 17:15:37 +0700

    FEAT: getset title ID

---
+++ pages/id/controller/settitle.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/">4.0.2</NuxtLink></li>
+          <li class=""><NuxtLink to="/id/controller/">Controller</NuxtLink></li>
+          <li class="unavailable">Metode setTitle</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">Metode setTitle</h1>
+            <p>
+              Metode <code>setTitle</code> digunakan untuk menyetel nilai title, sehingga ketika dipanggil oleh metode <NuxtLink to="/id/controller/gettitle/">getTitle</NuxtLink> tidak kosong.
+            </p>
+            <amp-img layout="responsive" width="674" height="158" :src="btiex" alt="Browser title example"></amp-img>
+
+            <h2>Bentuk Umum</h2>
+            <p>
+              Berikut ini adalah bentuk umum metode <code>setTitle</code> dari kelas <code>SENE_Controller</code>.
+            </p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this->setTitle(string $page_title): controllerObject
+                </highlight-code>
+              </div>
+            </div>
+
+            <h3>Parameter</h3>
+            <p>
+              Metode setTitle method memiliki 1 parameter wajib.
+            </p>
+
+            <h4>$page_title</h4>
+            <p>
+              Nilai dari parameter ini berjenis string dan dapat digunakan untuk memberi judul pada suatu halaman
+            </p>
+
+            <h2>Contoh Penggunaan</h2>
+            <p>Berikut ini adalah contoh kode penggunaan untuk metode <code>setTitle</code>.</p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  &#x3C;?php
+                  class Blog extends SENE_Controller {
+                    public function __construct(){
+                      parent::__construct();
+                    }
+                    public function index(){
+                      ...
+                      $this-&#x3E;setTitle(&#x27;Blog home&#x27;);
+                      ...
+                    }
+                    public function detail($id){
+                      ...
+                      $this-&#x3E;setTitle(&#x27;Blog Detail...&#x27;);
+                      ...
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
+
+            <div class="nav-bottom">
+              <div class="nav-bottom-left">
+                <nuxt-link to="/id/controller/puthemecontent/" class="btn">
+                  <i class="fa fa-chevron-left"></i>
+                  putThemeContent
+                </nuxt-link>
+              </div>
+              <div class="nav-bottom-right">
+                <nuxt-link to="/id/controller/resetthemecontent/" class="btn">
+                  resetThemeContent
+                  <i class="fa fa-chevron-right"></i>
+                </nuxt-link>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'id',
+  data (){
+    return {
+      name: 'Seme Framework v4.0.0',
+      suffix: ' - Seme Framework 4',
+      title: 'Metode setTitle',
+      description: 'Pelajari selengkapnya tentang metode setTitle dari SENE_Controller di Seme Framework 4.',
+      btiex: require('~/assets/img/controller/browser-title-example.png'),
+      breadcrumbs: [
+        {
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/',
+          text: 'id'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/id/controller/',
+          text: 'Controller'
+        }
+      ],
+    }
+  },
+  head() {
+    return {
+      htmlAttrs: {
+        lang: 'id'
+      },
+      title: this.title+this.suffix,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.name+': '+this.title
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.description
+        }
+      ]
+    }
+  },
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
+      },
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2021-07-25T17:15:00+07:00",
+        "datePublished": "2021-07-25T17:15:00+07:00",
+        "dateModified": "2021-07-25T17:15:00+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
+  }
+}
+</script>
