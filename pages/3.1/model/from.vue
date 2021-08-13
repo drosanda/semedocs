a548cdd84e6138d9810476b8fcf672364e866d18
daeng rosanda <daeng.rosanda@yahoo.co.id>
2021-08-14 00:53:32 +0700

    FIX: code highlight

--- pages/3.1/model/from.vue
+++ pages/3.1/model/from.vue
@@ -1,54 +1,229 @@
 <template>
-    <div class="section">
-        <div class="container">
-          <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul class="breadcrumbs">
-              <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
-              <li class=""><NuxtLink to="/model">Model</NuxtLink></li>
-            <li class="unavailable">From Method</li>
-          </ul>
-        </nav>
-        <div class="columns">
-          <div class="column">
-        <div class="content">
-          <h1 class="">From Method</h1>
-					<p><b>From</b> method is part of database class builder for selecting table with its alias.</p>
-					<h2>Parameters</h2>
-					<p>Update method has 2 required parameters that is <b>table name</b> and <b>table alias</b>. Table alias are required for using <b>join</b> and <b>join_method</b></p>
-					<pre>$this->db->from(string $table_name, string $table_alias): object</pre>
-          <h3>$table_name</h3>
-          <p><b>Table Name</b> refer to table name.</p>
-          <h3>$alias</h3>
-          <p><b>Alias</b> Aliased name of <b>$table_name</b>.</p>
-					<h2>Example usage</h2>
-					<p>Here is the examples using select method. See the first of this page for full example.</p>
-          <h3>Basic Usage</h3>
-          <p>For example we assumed want to add new data in blog table. First, in the model:</p>
-          <pre>
-class Blog_Model extends SENE_Model{
-  var $tbl = 'blog';
-  var $tbl_as = 'b';
-  public function __construct(){
-	 parent::__construct();
-  }
-  public function countList(){
-    $this->db->select_as("COUNT(*)","total",0);
-    $this->db->from($this->tbl,$this->tbl_as);
-    return $this->db->get_first();
-  }
-}</pre>
+  <div class="section">
+    <div class="container">
+      <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul class="breadcrumbs">
+          <li class=""><NuxtLink to="/">Seme Framework</NuxtLink></li>
+          <li class=""><NuxtLink to="/3.1">3.1.x</NuxtLink></li>
+          <li class=""><NuxtLink to="/3.1/model">Model</NuxtLink></li>
+          <li class="unavailable">From Method</li>
+        </ul>
+      </nav>
+      <div class="columns">
+        <div class="column">
+          <div class="content">
+            <h1 class="">From Method</h1>
+            <p>
+              <b>From</b> method is part of database class builder for selecting table with its alias.
+              This method support chaining method.
+            </p>
 
+            <h2>Basic Usage</h2>
+            <p>
+              Here is the basic usage <code>from</code> method from <code>$db</code> property on <NuxtLink to="/3.1/model/#SENE_Model">SENE_Model <i class="fa fa-window-restore"></i></NuxtLink> class.
+            </p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  $this-&#x3E;db-&#x3E;from(string $table_name[, string $table_alias=&#x22;&#x22;]): $this-&#x3E;db
+                </highlight-code>
+              </div>
+            </div>
+            <h3>Parameters</h3>
+            <p>This method has 1 required parameter and 1 optional parameter.</p>
+
+            <h4>$table_name</h4>
+            <p>
+              The <code>$table_name</code> value can contain string that refer to table name.
+            </p>
+
+            <h4>$alias</h4>
+            <p>
+              The <code>$alias</code> value can contain about string that aliased the value of <b>$table_name</b>.
+              This value are required if using the <NuxtLink to="/3.1/model/join/">Join Method <i class="fa fa-external-link"></i></NuxtLink>.
+            </p>
+
+            <h2>Example</h2>
+            <p>
+              Here is the example for <code>from</code> in a model class.
+            </p>
+            <div class="macwindow">
+              <div class="titlebar">
+                <div class="buttons">
+                  <div class="close">
+                    <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+                    <!-- close button link -->
+                  </div>
+                  <div class="minimize">
+                    <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+                    <!-- minimize button link -->
+                  </div>
+                  <div class="zoom">
+                    <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+                    <!-- zoom button link -->
+                  </div>
+                </div>
+              </div>
+              <div class="maccontent">
+                <highlight-code lang="php">
+                  class Blog_Model extends SENE_Model{
+                    var $tbl = &#x27;blog&#x27;;
+                    var $tbl_as = &#x27;b&#x27;;
+                    public function __construct(){
+                      parent::__construct();
+                    }
+                    public function countList(){
+                      $this-&#x3E;db-&#x3E;from($this-&#x3E;tbl,$this-&#x3E;tbl_as);
+                      return $this-&#x3E;db-&#x3E;get();
+                    }
+                  }
+                </highlight-code>
+              </div>
+            </div>
+
+            <div class="nav-bottom">
+              <div class="nav-bottom-left">
+                <nuxt-link to="/3.1/model/exec/" class="btn">
+                  <i class="fa fa-chevron-left"></i>
+                  Exec Method
+                </nuxt-link>
+              </div>
+              <div class="nav-bottom-right">
+                <nuxt-link to="/3.1/model/get_first/" class="btn">
+                  Get First Method
+                  <i class="fa fa-chevron-right"></i>
+                </nuxt-link>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
 
-        </div>
-      </div>
+
+
     </div>
+  </div>
 </template>
 <script>
-  export default {
-    layout: 'v3.1'
-    // page component definitions
+export default {
+  layout: 'v3.1',
+  data (){
+    return {
+      name: 'Seme Framework v3.1.5',
+      suffix: ' - Seme Framework 3.1.5',
+      title: 'From Method',
+      description: 'Learn about from method in $db property on SENE_Model class for Seme Framework 3.1.5.',
+      breadcrumbs: [
+        {
+          url: process.env.BASE_URL || 'http://localhost:3001',
+          text: 'Seme Framework'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/3.1',
+          text: '3.1.x'
+        },
+        {
+          url: (process.env.BASE_URL || 'http://localhost:3001')+'/3.1/model',
+          text: 'Model'
+        }
+      ],
+    }
+  },
+  head() {
+    return {
+      title: this.title+this.suffix,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: this.name+': '+this.title
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
+          content: this.description
+        }
+      ]
+    }
+  },
+  jsonld() {
+    this.breadcrumbs.push({url: (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path, text: this.title });
+    const items = this.breadcrumbs.map((item, index) => ({
+      '@type': 'ListItem',
+      position: index + 1,
+      item: {
+        '@id': item.url,
+        name: item.text,
+      },
+    }));
+    return [
+      {
+        '@context': 'https://schema.org',
+        '@type': 'BreadcrumbList',
+        itemListElement: items,
+      },
+      {
+        "@type": "NewsArticle",
+        "mainEntityOfPage": {
+          "@type": "WebPage",
+          "@id": (process.env.BASE_URL || 'http://localhost:3001')+this.$route.path.replace(/\/+$/, '') + '/'
+        },
+        "headline": (this.headline || this.title),
+        "image": [
+          (process.env.CDN_URL || 'http://localhost:3001')+'/logo.png'
+        ],
+        "dateCreated": "2021-08-14T00:51:01+07:00",
+        "datePublished": "2021-08-14T00:51:01+07:00",
+        "dateModified": "2021-08-14T00:51:01+07:00",
+        "author": {
+          "@type": "Person",
+          "gender": "Male",
+          "name": "Daeng Rosanda, S.Kom",
+          "alternateName": "Daeng Rosanda",
+          "jobTitle": "Founder",
+          "worksFor": {
+            "@type": "Organization",
+            "name": "Cipta Esensi Merenah",
+            "email": "hi@cenah.co.id"
+          }
+        },
+        "publisher": {
+          "@type": "Organization",
+          "name": "Cipta Esensi Merenah",
+          "description": "Cipta Esensi Merenah (Cenah) is software house company focused on developing web-based application from Bandung, Indonesia.",
+          "logo": {
+            "@type": "ImageObject",
+            "name": "logo Cipta Esensi Merenah",
+            "url": "https://cdn.cenah.co.id/_nuxt/img/logo-wide.5420183.png",
+            "width": "256px",
+            "height": "62px"
+          }
+        },
+        "description": this.description
+      }
+    ];
   }
+}
 </script>
-
