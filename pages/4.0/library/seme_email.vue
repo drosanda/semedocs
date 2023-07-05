2188aed7083d888b66a26f63ecf4b37efed0d1f1
Daeng R [REAL] <drosanda@outlook.co.id>
2023-07-05 23:47:21 +0700

    FEAT: seme_email docs

--- pages/4.0/library/seme_email.vue
+++ pages/4.0/library/seme_email.vue
@@ -14,9 +14,9 @@
           <div class="content">
             <h1 class="">Seme Email Library</h1>
             <p>The <code>Seme_Email</code> library purpose is for sending email from Seme Framework. Seme Email has HTML email template for dynamic content of html.</p>
-            <hr>
-            <h2>How to use</h2>
-            <p>First we have to loaded library by using library loaded:</p>
+            
+            <h2>Class Structure</h2>
+            <p>Here is the Structure of Seme Email Class.</p>
             <div class="macwindow">
               <div class="titlebar">
                 <div class="buttons">
@@ -36,13 +36,47 @@
               </div>
               <div class="maccontent">
                 <highlight-code lang="php">
-                  $this-&#x3E;lib(&#x27;seme_email&#x27;);
+                  &#x3C;?php
+                  Class Seme_Email {
+                    public $log;
+
+                    public function __construct() {}
+
+                    public function from($from_email_address[, $name='']): Seme_Email {}
+
+                    public function to($to_email_address[, $name='']): Seme_Email {}
+
+                    public function subject($title_or_subject_email): Seme_Email {}
+
+                    public function text($ascii_text): Seme_Email {}
+
+                    public function html($html_content): Seme_Email {}
+
+                    public function send(): Seme_Email {}
+
+                    public function template($file_template_php): Seme_Email {}
+
+                    public function replacer($array_of_string): Seme_Email {}
+
+                    public function attachment_add($filepath[, $filename='']): Seme_Email {}
+
+                    public function attachment_clear(): Seme_Email {}
+
+                    public function getLog(): string {}
+
+                    public function preview(): void {}
+                  }
                 </highlight-code>
               </div>
             </div>
-            <p>You can put libary loader in constructor of controller or on each method in your controller.</p>
+            <hr>
+
             <h2>Basic Usage</h2>
-            <p>Seme Email has some method, here is the list of common methods:</p>
+            <p>
+              You can put libary loader in constructor of controller or on each method in your controller.
+              To use the Seme Email library, you can add a few lines of code to a controller in the Seme Framework. 
+              The following is a basic example of implementing the Seme Email code in a Controller.
+            </p>
             <div class="macwindow">
               <div class="titlebar">
                 <div class="buttons">
@@ -62,29 +96,28 @@
               </div>
               <div class="maccontent">
                 <highlight-code lang="php">
-                  Class Product extends SEME_Controller{
+                  &#x3C;?php
+                  Class EmailRegister extends \SENE_Controller{
                     public function __construct(){
-                      $this-&#x3E;lib(&#x27;seme_log&#x27;);
+                      $this-&#x3E;lib('seme_email');
                     }
                     public function index(){
-                      $replacer = array();
-                      $replacer[&#x27;site_name&#x27;] = $this-&#x3E;site_name;
-                      $replacer[&#x27;nama&#x27;] = $nama;
-                      $replacer[&#x27;activation_link&#x27;] = $link;
-                      $this-&#x3E;seme_email-&#x3E;flush(); //optional
-                      $this-&#x3E;seme_email-&#x3E;replyto($this-&#x3E;site_name,$this-&#x3E;site_replyto);
-                      $this-&#x3E;seme_email-&#x3E;from($this-&#x3E;site_email,$this-&#x3E;site_name);
-                      $this-&#x3E;seme_email-&#x3E;subject(&#x27;Registration Successful&#x27;);
-                      $this-&#x3E;seme_email-&#x3E;to($email,$nama);
-                      $this-&#x3E;seme_email-&#x3E;template(&#x27;account_register&#x27;);
-                      $this-&#x3E;seme_email-&#x3E;replacer($replacer);
+                      $this-&#x3E;seme_email-&#x3E;flush();
+                      $this-&#x3E;seme_email-&#x3E;replyto('Support Example Website', 'support@cenah.co.id');
+                      $this-&#x3E;seme_email-&#x3E;from('noreply@example.com', 'Adry from Example Website');
+                      $this-&#x3E;seme_email-&#x3E;subject('Registration Successful');
+                      $this-&#x3E;seme_email-&#x3E;to('agus.setiawan@example.com', 'Agus Setiawan');
+                      $this-&#x3E;seme_email-&#x3E;text('Thank you for registering on Example website.');
                       $this-&#x3E;seme_email-&#x3E;send();
+
+                      // optional, verbose the process
+                      dd($this-&#x3E;seme_email-&#x3E;getLog());
                     }
                   }
                 </highlight-code>
               </div>
             </div>
-            <p>Seme Email has several methods that can used for sending email properly.</p>
+            <p>To run the process of sending email, you can simply call this class via the browser. Example: http://localhost/seme_framework/emailregister.</p>
             <h3>From Method</h3>
             <p>This method mandatory for declare origin of email. Required two parameter, first parameter is email and the second parameter is name of sender.</p>
             <h3>Subject Method</h3>
@@ -92,7 +125,10 @@
             <h3>To Method</h3>
             <p>This method mandatory for declare target of email. Required two parameter, first parameter is email and the second parameter is name of sender. Can be called more than once for sending multiple email address.</p>
             <h3>Template Method</h3>
-            <p>This method only required if you one to create html formatted email. Default location about this file is in kero/lib/seme_email/template/. You can replace the string with "replacer" for creating dynamic email. Here is the example:</p>
+            <p>
+              This method only required if you one to create html formatted email. Default location about this file is in <code>kero/lib/seme_email/template</code>. 
+              You can replace the string by using replacer method for creating dynamic email. Here is the example of HTML email template:
+            </p>
 
             <div class="macwindow">
               <div class="titlebar">
@@ -112,11 +148,12 @@
                 </div>
               </div>
               <div class="maccontent">
-                <highlight-code lang="php">
+                <highlight-code lang="html">
                   &#x3C;html&#x3E;
                   &#x3C;body&#x3E;
-                  &#x3C;p&#x3E;Hi {{first_name}},&#x3C;/p&#x3E;
-                  &#x3C;p&#x3E;Thank you for using seme framework&#x3C;/p&#x3E;
+                  &#x3C;p&#x3E;Hi {{full_name}},&#x3C;/p&#x3E;
+                  &#x3C;p&#x3E;Thank you for using registering at {{site_name}}.&#x3C;/p&#x3E;
+                  &#x3C;p&#x3E;You can clicking on this link {{dashboard_link}} to go to your dashboard.&#x3C;/p&#x3E;
                   &#x3C;/body&#x3E;
                   &#x3C;/html&#x3E;
                 </highlight-code>
@@ -145,15 +182,37 @@
               </div>
               <div class="maccontent">
                 <highlight-code lang="php">
-                  $replacer = array();
-                  $replacer[&#x27;first_name&#x27;] = &#x27;Agus Setiawan&#x27;;
-                  $this-&#x3E;seme_email-&#x3E;replacer($replacer);
+                &#x3C;?php
+                Class EmailRegister extends \SENE_Controller{
+                  public function __construct(){
+                    $this-&#x3E;lib('seme_email');
+                  }
+                  public function index(){
+                    $replacer = array();
+                    $replacer['site_name'] = 'Example Website';
+                    $replacer['full_name'] = 'Agus Setiawan';
+                    $replacer['dashboard_link'] = base_url('dashboard/');
+
+                    $this-&#x3E;seme_email-&#x3E;flush();
+                    $this-&#x3E;seme_email-&#x3E;replyto('Support Example Website', 'support@cenah.co.id');
+                    $this-&#x3E;seme_email-&#x3E;from('noreply@example.com', 'Adry from Example Website');
+                    $this-&#x3E;seme_email-&#x3E;subject('Registration Successful');
+                    $this-&#x3E;seme_email-&#x3E;to('agus.setiawan@example.com', 'Agus Setiawan');
+                    $this-&#x3E;seme_email-&#x3E;template('account_register');
+                    $this-&#x3E;seme_email-&#x3E;replacer($replacer);
+                    $this-&#x3E;seme_email-&#x3E;send();
+
+                    // optional, verbose the process
+                    dd($this-&#x3E;seme_email-&#x3E;getLog());
+                  }
+                }
                 </highlight-code>
               </div>
             </div>
             <p>If this example combined with email template above, you can send email with <b>{{first_name}}</b> replaced by <b>Agus Setiawan</b>.</p>
             <h3>Flush method</h3>
             <p>Use this method for clearing assigned properties used before.</p>
+            <hr />
             <h2>Special Notes</h2>
             <p>Please makes more attention if you want to send email by following community guidance for email usage. If your email always sent to spam, you have to use real domain and real working email.</p>
             <p>Google has completed guide for email usage. Here is the quick explanation:</p>
@@ -178,6 +237,9 @@ export default {
   layout: 'v4.0',
   data() {
     return {
+      full_name: '{{full_name}}',
+      site_name: '{{site_name}}',
+      dashboard_link: '{{dashboard_link}}',
       name: 'Seme Framework 4',
       suffix: ' - Seme Framework 4',
       title: 'Seme_Email Library',
@@ -249,7 +311,7 @@ export default {
         ],
         "dateCreated": "2021-08-11T19:52:38+07:00",
         "datePublished": "2021-08-11T19:52:38+07:00",
-        "dateModified": "2021-06-11T01:04:00+07:00",
+        "dateModified": "2023-07-04T23:46:00+07:00",
         "author": {
           "@type": "Person",
           "gender": "Male",
