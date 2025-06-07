e904baef6b21e9c8064b3eed6065c2b07334277c
Daeng Rosanda <daeng.rosanda@yahoo.co.id>
2025-06-07 23:54:43 +0700

    FEAT: markdown code snippet component

---
+++ components/MacWindowCodeSnippet.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="macwindow">
+    <div class="titlebar">
+      <div class="buttons">
+        <div class="close">
+          <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+        </div>
+        <div class="minimize">
+          <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+        </div>
+        <div class="zoom">
+          <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+        </div>
+      </div>
+    </div>
+    <div class="maccontent">
+      <highlight-code :lang="lang">
+        <slot />
+      </highlight-code>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lang: {
+      type: String,
+      default: 'html'
+    }
+  }
+}
+</script>
